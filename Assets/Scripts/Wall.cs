40e7b96be8599e5468c37bfb4fda5c32f4e38f53
XiaoyaWan <wannxiaoya970921@gmail.com>
2020-05-28 20:42:11 -0700

    Initial commit

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 50;
+        gameObject.transform.localPosition = Vector3.MoveTowards(gameObject.transform.localPosition, new Vector3(123, 231, -323), speed * Time.deltaTime);
+        if (gameObject.transform.localPosition== new Vector3(123, 231, -323) ) 
+        {
+            if (ifClear())
+            {
+                gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0, 350);
+                CreateShape();
+                ChangeFixedObject();
+                ChangeMovableCube();
+            }
+            else 
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            
+        }
+    
+    }
+
+    public void CreateShape()
+    {
+
+    }
+
+    public void ChangeFixedObject()
+    {
+
+    }
+
+    public void ChangeMovableCube()
+    {
+
+    }
+
+    public Boolean ifClear()
+    {
+        return false;
+    }
+
+}
